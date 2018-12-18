6eba5e8ead7f4e09c04b50d320fe2c8db16f18d4
akeller98 <aaronkeller2021@u.northwestern.edu>
2018-12-18 14:07:11 -0600

    Installed react-webcam and implemented it into webapp. Need to send data to express. 12/18/18 @ 2:07 PM

--- spotify-playlist-generator/src/App.js
+++ spotify-playlist-generator/src/App.js
@@ -2,24 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom';
+import  Webcam  from 'react-webcam'
+
+export class WebcamCapture extends Component{
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+  };
+  render(){
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: "user"
+    };
+    return(
+      <div style={{position:"relative", left:0, top: 20}}>
+        <Webcam
+          audio = {false}
+          height={480}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={640}
+          videoConstraints={videoConstraints}
+        />
+        <button onClick={this.capture} style={{position:"relative", left:-380}}>Capture photo</button>
+      </div>
+    );
+  }
+}
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <img src={logo} className="App-logo" alt="logo"  style={{width: 300, position: "absolute", top: 0}}/>
+          <WebcamCapture/>
+          <p style={{position: "absolute", top: 750}}>
+            Click button to generate playlist.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
