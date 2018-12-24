3f1c16248b00efbdc0a275e0e2f812f1997de622
akeller98 <aaronkeller2021@u.northwestern.edu>
2018-12-23 22:03:35 -0600

    Got rid of unnecessary files (App.js and App.css) and transferred everything to Index files. 12/23/18 @ 10:03PM

--- spotify-playlist-generator/src/index.js
+++ spotify-playlist-generator/src/index.js
@@ -1,16 +1,89 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import './App.css';
 import './index.css';
-import { App } from './App.js';
-//import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';
+import  Webcam  from 'react-webcam'
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+export class WebcamCapture extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      processed_results : 'test'
+    };
+    this.capture = this.capture.bind(this);
+    this.setRef = this.setRef.bind(this);
+    //this.setVal = this.set.bind(this);
+  }
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+  capture () {
+    const imageSrc = this.webcam.getScreenshot();
+    let data = {
+      "image": imageSrc
+    }
+    let results = '';
+    fetch('/process', {
+      method: "POST",
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(function(response){
+      return response.body;
+    })
+
+    setTimeout(function(){
+    fetch('/process')
+      .then(res => res.json())
+      .then(data => results = JSON.stringify(data))
+    }, 2000);
+
+    setTimeout(
+      () => this.setState({processed_results: results})
+    , 3000);
+    //this.forceUpdate();
+  };
+  render(){
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: "user"
+    };
+    return(
+      <div style={{position:"relative", left:0, top: 20}}>
+        <Webcam
+          audio = {false}
+          height={480}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={640}
+          videoConstraints={videoConstraints}
+        />
+        <button onClick={this.capture} style={{position:"relative", left:-380}}>Capture photo</button>
+        <p> {this.state.processed_results} </p>
+      </div>
+    );
+  }
+}
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"  style={{width: 300, position: "absolute", top: 0}}/>
+          <WebcamCapture/>
+          <p style={{position: "absolute", top: 750}}>
+            Click button to generate playlist.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
